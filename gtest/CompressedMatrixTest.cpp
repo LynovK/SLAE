aa07d3e49a0685b1fb6aea86602af450cbc7c152
Perseverrance <vegastarblaze@mail.ru>
2023-03-09 22:23:47 +0300

    CSR test push

--- gtest/CompressedMatrixTest.cpp
+++ gtest/CompressedMatrixTest.cpp
@@ -1,23 +1,22 @@
 //
 // Created by perseverance on 05.03.23.
 //
+
 #include <gtest/gtest.h>
-#include "../src/Compressed_spars_row/CompressedConverterMatrixWithZEROS.h"
+#include "../src/Compressed_spars_row/CompressedMatrix.h"
 #include "../src/Compressed_spars_row/CompressedConverter.h"
 
 
-TEST(CompressedTest, zeroTest){
+TEST(CompressedTest, firstTest){
     std::vector<element<int>> matrix = {{0,0,1}, {0,1,2},
                                         {1,1,1}, {1,2,3},
                                         {2,0,4}, {2,2,2}};
     sort_me_plz(matrix);
-    CompressedMatrix res = represent(matrix, 3, 3);
-
+    CompressedMatrix<int> res (matrix, 3, 3);
 
     std::vector<float> vector_of_values = {1, 2, 1, 3, 4, 2};
     std::vector<unsigned int> colomn_index_vector = {0, 1, 1, 2, 0, 2};
     std::vector<unsigned int> counts_vector = {0, 2, 4, 6};
-    CompressedMatrix testMatr(vector_of_values, colomn_index_vector, counts_vector);
 
     for (int i = 0; i < vector_of_values.size(); i++){
         ASSERT_NEAR(vector_of_values[i], res.getValue()[i], 0.001);
@@ -28,19 +27,19 @@ TEST(CompressedTest, zeroTest){
     }
 }
 
-TEST(CompressedTest, firstTest){
-    std::vector<element<int>> matrix = {{0,0,1}, {0,1,2}, {0,2,0},
-                                        {1,0,0}, {1,1,1}, {1,2,3},
-                                        {2,0,4}, {2,1,0}, {2,2,2}};
-
+TEST(CompressedTest, secondTest){
+    std::vector<element<float>> matrix = {{0,0,1}, {0,1,2},
+                                        {1,1,1}, {1,2,3},
+                                        {2,0,4}, {2,2,2}};
     sort_me_plz(matrix);
-    CompressedMatrix res = represent_with_zeros(matrix, 3, 3);
-
+    CompressedMatrix<float> res (matrix, 3, 3);
 
     std::vector<float> vector_of_values = {1, 2, 1, 3, 4, 2};
     std::vector<unsigned int> colomn_index_vector = {0, 1, 1, 2, 0, 2};
     std::vector<unsigned int> counts_vector = {0, 2, 4, 6};
-    CompressedMatrix testMatr(vector_of_values, colomn_index_vector, counts_vector);
+    std::vector<float> x = {1, 3, 5};
+    std::vector<float> solve = {7, 8, 14};
+    std::vector<float> result = res.dot(x);
 
     for (int i = 0; i < vector_of_values.size(); i++){
         ASSERT_NEAR(vector_of_values[i], res.getValue()[i], 0.001);
@@ -49,9 +48,11 @@ TEST(CompressedTest, firstTest){
     for(int i = 0; i < counts_vector.size(); ++i){
         ASSERT_NEAR(counts_vector[i], res.get_count()[i], 0.001);
     }
+
 }
 
-TEST(CompressedTest, zeroshtrihTest){
+
+TEST(CompressedTest, thirdTest){
     std::vector<element<float>> matrix = {{0,0,15}, {0,1,1.2}, {0,4,1},
                                           {1,3,1}, {1,4,2},
                                           {2,1,3}, {2,2,4}, {2,4,7},
@@ -59,95 +60,17 @@ TEST(CompressedTest, zeroshtrihTest){
                                           {4,0,5.8}, {4,1,3},{4,4,9}
     };
     sort_me_plz(matrix);
-    CompressedMatrix res = represent(matrix, 5, 5);
+    CompressedMatrix res(matrix, 5, 5);
 
 
     std::vector<float> vector_of_values = {15, 1.2, 1, 1, 2, 3, 4, 7, 1, 2, 1, 5.8, 3, 9};
     std::vector<unsigned int> colomn_index_vector = {0, 1, 4, 3, 4, 1, 2, 4, 0, 1, 3, 0, 1, 4};
     std::vector<unsigned int> counts_vector = {0, 3, 5, 8, 11, 14};
-    CompressedMatrix testMatr(vector_of_values, colomn_index_vector, counts_vector);
-
     for (int i = 0; i < vector_of_values.size(); i++){
         ASSERT_NEAR(vector_of_values[i], res.getValue()[i], 0.001);
         ASSERT_NEAR(colomn_index_vector[i], res.get_index()[i], 0.001);
     }
     for(int i = 0; i < counts_vector.size(); ++i){
         ASSERT_NEAR(counts_vector[i], res.get_count()[i], 0.001);
-    }
-}
-
-TEST(CompressedTest, SecondTest){
-    std::vector<element<int>> matrix = {{0,0,5}, {0,1,0}, {0,2,6},
-                                   {1,0,1}, {1,1,2}, {1,2,0},
-                                   {2,0,0}, {2,1,0}, {2,2,1}};
-
-
-    sort_me_plz(matrix);
-    CompressedMatrix res = represent_with_zeros(matrix, 3, 3);
-
-
-    std::vector<float> vector_of_values = {5, 6, 1, 2, 1};
-    std::vector<unsigned int> colomn_index_vector = {0, 2, 0, 1, 2};
-    std::vector<unsigned int> counts_vector = {0, 2, 4, 5};
-    CompressedMatrix testMatr(vector_of_values, colomn_index_vector, counts_vector);
-
-    for (int i = 0; i < vector_of_values.size(); i++){
-        ASSERT_NEAR(vector_of_values[i], res.getValue()[i], 0.001);
-        ASSERT_NEAR(colomn_index_vector[i], res.get_index()[i], 0.001);
-    }
-    for(int i = 0; i < counts_vector.size(); ++i){
-        ASSERT_NEAR(counts_vector[i], res.get_count()[i], 0.001);
-    }
-}
-
-
-
-TEST(CompressedTest, ThirdTest){
-    std::vector<element<float>> matrix = {{0,0,15}, {0,1,1.2}, {0,2,0}, {0,3,0}, {0,4,1},
-                                   {1,0,0}, {1,1,0}, {1,2,0}, {1,3,1}, {1,4,2},
-                                   {2,0,0}, {2,1,3}, {2,2,4}, {2, 3, 0}, {2,4,7},
-                                   {3,0,1}, {3,1,2}, {3,2,0}, {3,3,1}, {3,4,0},
-                                   {4,0,5.8}, {4,1,3}, {4,2,0}, {4,3,0}, {4,4,9}
-    };
-
-    sort_me_plz(matrix);
-    CompressedMatrix res = represent_with_zeros(matrix, 5, 5);
-
-
-    std::vector<float> vector_of_values = {15, 1.2, 1, 1, 2, 3, 4, 7, 1, 2, 1, 5.8, 3, 9};
-    std::vector<unsigned int> colomn_index_vector = {0, 1, 4, 3, 4, 1, 2, 4, 0, 1, 3, 0, 1, 4};
-    std::vector<unsigned int> counts_vector = {0, 3, 5, 8, 11, 14};
-    CompressedMatrix testMatr(vector_of_values, colomn_index_vector, counts_vector);
-
-    for (int i = 0; i < vector_of_values.size(); i++){
-        ASSERT_NEAR(vector_of_values[i], res.getValue()[i], 0.001);
-        ASSERT_NEAR(colomn_index_vector[i], res.get_index()[i], 0.001);
-    }
-    for(int i = 0; i < counts_vector.size(); ++i){
-        ASSERT_NEAR(counts_vector[i], res.get_count()[i], 0.001);
-    }
-}
-
-
-TEST(CompressedTest, FirstSolveTest){
-    std::vector<element<int>> matrix = {{0,0,1}, {0,1,2}, {0,2,0},
-                                   {1,0,0}, {1,1,4}, {1,2,0},
-                                   {2,0,0}, {2,1,2}, {2,2,6}};
-
-    sort_me_plz(matrix);
-    CompressedMatrix res = represent_with_zeros(matrix, 3, 3);
-
-
-    std::vector<int> vector_of_values = {1, 2, 1, 3, 4, 2};
-    std::vector<unsigned int> colomn_index_vector = {0, 1, 1, 2, 0, 2};
-    std::vector<unsigned int> counts_vector = {0, 2, 4, 6};
-    CompressedMatrix testMatr(vector_of_values, colomn_index_vector, counts_vector);
-
-    std::vector<int> x = {1, 3, 5};
-    std::vector<int> d = {7, 12, 36};
-    std::vector<int> solve;
-    solve = dot(x, res);
-    for(auto i = 0; i < solve.size(); ++i){
-        ASSERT_NEAR(d[i], solve[i], 0.001);
     }
 }
--- src/Compressed_spars_row/CompressedConverter.h
+++ src/Compressed_spars_row/CompressedConverter.h
@@ -9,53 +9,13 @@
 #include <algorithm>
 #include <vector>
 
+
 template<typename T>
 void sort_me_plz(std::vector<element<T>> &matrix_){
-    std::sort(matrix_.begin() + 2, matrix_.end());
-}
-
-template <typename T>
-CompressedMatrix<T> represent(std::vector<element<T>> matrix_, unsigned int n, unsigned int m) noexcept{
-    std::vector<T> vector_of_values;
-    std::vector<unsigned int> colomn_index_vector;
-    std::vector<unsigned int> counts_vector;
-    std::vector<element<T>> non_zero_triple;
-
-    for(auto i = 0; i < matrix_.size(); ++i){
-        vector_of_values.push_back(matrix_[i].v);         //Получаем строку с ненулевыми элементами матрицы
-        colomn_index_vector.push_back(matrix_[i].j);      //Получаем строку с номерами столбцов
-        non_zero_triple.push_back(matrix_[i]);
-    }
-
-    unsigned int count = 0;
-    counts_vector.push_back(0);
-    for(auto k = 0; k < n; ++k){
-        for(auto p = 0; p < non_zero_triple.size(); ++p){     // Вектор с количеством ненулевых элементов построчно
-            if(non_zero_triple[p].i == k){
-                count++;
-            }
-        }
-        counts_vector.push_back(count);
-    }
-
-    CompressedMatrix result(vector_of_values, colomn_index_vector, counts_vector);
-    return result;
-}
-
-template <typename T>
-std::vector<T> dot(const std::vector<T> &X, const CompressedMatrix<T> &CSR) noexcept{
-    std::vector<T> x;
-    for(auto i = 0; i < CSR.get_count().size() - 1; ++i){
-        unsigned int k = CSR.get_count()[i + 1] - CSR.get_count()[i];
-        unsigned int b = CSR.get_count()[i];
-        x.push_back(0);
-        for(auto j = 0; j < k; ++j){
-            x[i] += CSR.getValue()[b + j]*X[CSR.get_index()[b + j]];
-        }
-    }
-    return x;
+    std::sort(matrix_.begin(), matrix_.end(), [](const element<T> &first_element, const element<T> &second_element ){
+        if (first_element.i != second_element.i) return first_element.i < second_element.i;
+        if (first_element.i == second_element.i) return first_element.j < second_element.j;
+    });
 }
 
 #endif //SLAE_COMPRESSEDCONVERTER_H
-
-//hello
